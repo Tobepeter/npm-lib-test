336725dd70c6955f37fff6f16551e8c1f5e04d93
tobe <583590744@qq.com>
2025-04-19 19:28:16 +0800

    canvas test

--- src/canvas/canvas-test.js
+++ src/canvas/canvas-test.js
@@ -1,6 +1,7 @@
 import { Canvas } from 'canvas'
 import fs from 'fs'
 import path from 'path'
+import { execSync } from 'child_process'
 import { dirname } from 'dirname-filename-esm'
 
 const __dirname = dirname(import.meta)
@@ -18,16 +19,26 @@ function test_canvas() {
     }
   }
 
-  // === Get image buffer and print ===
   const buffer = canvas.toBuffer()
   // console.log('Chessboard image buffer:', buffer)
 
-  // === Save image to file ===
+  // === Save ===
   fs.writeFileSync(path.join(__dirname, 'chessboard.png'), buffer)
+
+  // === ImageData ===
+  const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height)
+  console.log('ImageData:', imageData)
+}
+
+function test_hexdump() {
+  // NOTE: canvas转buffer其实是附带png的元数据的，不是纯粹的位图信息
+  const result = execSync(`hexdump -C ${path.join(__dirname, 'chessboard.png')} | head -n 3`)
+  console.log(result.toString())
 }
 
 function main() {
   test_canvas()
+  test_hexdump()
 }
 
 main()
